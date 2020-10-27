a402b59f8c8c67619c468b0d34520f02eb121a79
hanlee <me@hanlee.co>
2020-10-27 15:36:58 +0900

    feat: menu page

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,28 +1,18 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
-    </ion-content>
-  </ion-page>
+  <PageWrapper name="出席確認">
+    <MenuList></MenuList>
+  </PageWrapper>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import PageWrapper from '@/components/PageWrapper.vue';
+import MenuList from '@/components/MenuList.vue';
 
 export default  {
-  name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  name: 'Tab1',
+  components: {
+    PageWrapper,
+    MenuList,
+  }
 }
 </script>
